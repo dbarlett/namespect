304133b0716be06b278fe3b64a2cfd1a0a5ae98c
Dylan Barlett <dylan.barlett@gmail.com>
2020-07-30 19:20:20 -0400

    Update NPI download script. See #5.

--- data/download_npi.sh
+++ data/download_npi.sh
@@ -3,6 +3,6 @@
 
 mkdir -p npi
 cd npi
-wget --no-clobber http://download.cms.gov/nppes/NPPES_Data_Dissemination_November_2016.zip
-unzip -u NPPES_Data_Dissemination_November_2016.zip
-tail --lines=+2 --quiet npidata_20050523-20161113.csv | cut --delimiter "," --fields=6,7,42
+wget --no-clobber https://download.cms.gov/nppes/NPPES_Data_Dissemination_July_2020.zip
+unzip -u NPPES_Data_Dissemination_July_2020.zip
+tail --lines=+2 --quiet npidata_pfile_20050523-20200712.csv | cut --delimiter "," --fields=6,7,42 | grep --invert-match '"","",""' > NPI.csv
